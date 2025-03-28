fbc3fa9aaa42d4aa7d9f6a1554c6e86c4b580968
iti24 <itishreepanda.1998@gmail.com>
2025-03-28 14:40:51 +0530

    default port number in config file

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const config = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 4000,
   mongoURL: process.env.MONGODB_URL,
   jwtSecret: process.env.JWT_SECRET || "fallbackSecretKey",
 };
